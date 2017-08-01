bf1f8aa71294f543ab1efaab1c7c4bbda700e39b
Jubil <valentin.53@orange.fr>
2017-08-02 00:22:32 +0200

    HeightMapExemple

---
+++ src/main/java/fr/jubil/map/exemple/HeightMapExemple.java
@@ -0,0 +1,13 @@
+package fr.jubil.map.exemple;
+
+import fr.jubil.map.bean.HeightMap;
+
+public class HeightMapExemple {
+
+	public static void main(String[] args) {
+
+		HeightMap cm = new HeightMap("HeightMapExemple");
+		cm.exportAsPNG();
+
+	}
+}
